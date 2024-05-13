136ccc56ea4707762cf5722622c76013fd643d1f
darrylpoh <darrylpoh.dp13@gmail.com>
2024-05-13 14:51:39 +0800

    Report skeleton

--- classes/instrument.py
+++ classes/instrument.py
@@ -28,4 +28,33 @@ class Instrument:
         self.day_high = max(self.day_high, price) if self.day_high is not None else price
         self.day_low = min(self.day_low, price) if self.day_low is not None else price
 
+    def generate_report(self):
+        """
+        Generate instrument report
+        """
 
+        fields = ['Instrument ID', 'OpenPrice', 'ClosePrice', 'TotalVolume', 'VWAP', 'DayHigh', 'DayLow']
+
+        instrumentRows = []
+
+        total_price_volume = sum([price * volume for price, volume in self.matchings]) # need jareds matching
+        vwap = round(total_price_volume / self.total_traded_volume, 4)
+
+        instrumentRows.push({
+            'Instrument ID': self.instrumentID,
+            'OpenPrice': self.open_price,
+            'ClosePrice': self.closed_price,
+            'TotalVolume': self.total_traded_volume,
+            'VWAP': vwap,
+            'DayHigh': self.day_high,
+            'DayLow': self.day_low
+        })
+
+        filename = "output_instrument_report.csv"
+
+        with open(filename, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fields)
+    
+            writer.writeheader(fields)
+
+            writer.writerows(instrumentRows)
